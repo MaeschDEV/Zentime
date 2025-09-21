ef7e7c29c5a0d02677c0fffde6f998d20f813f23
Malte Esch <malteesch1508@gmail.com>
2025-09-21 14:19:30 +0200

    Addded Cocoapods

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:zentime/screens/dashboard_screen.dart';
 import 'package:zentime/screens/settings_screen.dart';
 import 'package:zentime/screens/statistics_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
