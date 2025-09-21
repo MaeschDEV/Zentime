bfd57de9fce049b291d8bd98a0d50e5981ad74ab
Malte Esch <malteesch1508@gmail.com>
2025-09-21 15:01:29 +0200

    Added a tutorial for the strategy with saving

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,47 @@ import 'package:zentime/screens/statistics_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  await Hive.openBox('days');
+
+  /*
+  Tutorial
+
+  First: WidgetsFlutterBinding.ensureInitialized();
+  -> Ensures that everything is initialized
+
+  Then:
+  await Hive.initFlutter();
+  await Hive.openBox('days');
+  -> Start hive and open the database
+
+  Create a key for the current day. Use:
+  String todayKey = DateTime(2025, 09, 21).toIso8601String().split('T').first;
+  -> Only the day gets saved, not the time as well
+
+  Check-In:
+  Create a DayEntry with the current day and with the correct sick and holiday bools
+  Create a workSession with the start and end set to DateTime.now()
+  Create an empty breakSession []
+  Use:
+  await Hive.box('days').put(todayKey, today.toJson());
+  to save
+
+  Break clicked:
+  Load the DayEntry with:
+  today = DayEntry.fromJson(Hive.box('days').get(todayKey));
+
+  Edit the last worksession and set the end to now, leave the start as it was
+  Create a new breakSession with the start and end set to now.
+  Save the DayEntry to hive
+
+  Work clicked:
+  Exactly like break, but edit the breaksession and create a new worksession
+
+  Check-Out clicked:
+  Edit the last worksession and set the end to now
+  If the last session was a breaksession, edit the breaksession
+  */
+
   runApp(const MyApp());
 }
 
